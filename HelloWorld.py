142f4d0d5d204bba8f391cafbca9fe67715d6e0e
qpo <janlubatschowski@gmail.com>
2015-05-07 14:32:39 +0200

    And another try, just getting familiar with committing stuff

--- HelloWorld.py
+++ HelloWorld.py
@@ -2,7 +2,7 @@ my_list = [i**2 for i in range(1,11)]
 # Generates a list of squares of the numbers 1 - 10
 poker_hand_1 = "Royal Flush"
 f = open("output.txt", "w")
-
+input = open("train.csv")
 for item in my_list:
     f.write(str(item) + "\n")
 	
